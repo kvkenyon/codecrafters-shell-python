63e5c18d6b6d2fd215e861aa4ef5b86ea8d86ee2
Kevin <kvkenyon@gmail.com>
2025-02-13 10:57:12 -0600

    add path to type cmd

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 SHELL_BUILTIN = lambda x: f"{x} is a shell builtin"  # noqa
 
@@ -6,8 +7,11 @@ BUILTINS = set(["echo", "exit", "type"])
 
 CMDS = set(["echo", "type", "exit"])
 
+PATH = os.environ["PATH"]
+
 
 def main():
+    paths = PATH.split(":")
     while True:
         sys.stdout.write("$ ")
         cmd_raw = input()
@@ -20,6 +24,16 @@ def main():
         elif cmd_name == "echo":
             print(" ".join(args))
         elif cmd_name == "type":
+            found = False
+            for path in paths:
+                subcmd = path + "/" + args[0]
+                if os.path.exists(subcmd):
+                    print(f"{args[0]} is {subcmd}")
+                    found = True
+                    break
+            if found:
+                continue
+
             if args[0] not in BUILTINS:
                 print(f"{args[0]}: not found")
                 continue
