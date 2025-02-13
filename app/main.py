1cf337ae2ebf0925cd2f208e9dddd3b2968c1233
Kevin <kvkenyon@gmail.com>
2025-02-13 10:40:55 -0600

    echo cmd

--- app/main.py
+++ app/main.py
@@ -2,18 +2,19 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
-
-    valid_cmds = []
-
     while True:
-        # Wait for user input
         sys.stdout.write("$ ")
-        cmd = input()
-        if cmd == "exit 0":
+        cmd_raw = input()
+        cmd = cmd_raw.split(" ")
+        if not cmd:
+            return f"{cmd_raw}: command not found"
+        cmd_name, args = cmd[0], cmd[1:]
+        if cmd_name == "exit":
             sys.exit(0)
-        if cmd not in valid_cmds:
-            print(f"{cmd}: command not found")
+        elif cmd_name == "echo":
+            print(" ".join(args))
+        else:
+            print(f"{cmd_raw}: command not found")
 
 
 if __name__ == "__main__":
