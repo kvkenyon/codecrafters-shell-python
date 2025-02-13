f532656bcf65c9d0b26bb4c5a54402540ce10429
Kevin <kvkenyon@gmail.com>
2025-02-13 10:46:20 -0600

    type cmd

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 import sys
 
+SHELL_BUILTIN = lambda x: f"{x} is a shell built in"  # noqa
+
+BUILTINS = set(["echo", "exit", "type"])
+
+CMDS = set(["echo", "type", "exit"])
+
 
 def main():
     while True:
@@ -13,6 +19,12 @@ def main():
             sys.exit(0)
         elif cmd_name == "echo":
             print(" ".join(args))
+        elif cmd_name == "type":
+            if args[0] not in BUILTINS:
+                print(f"{args[0]}: not found")
+                continue
+            print(SHELL_BUILTIN(args[0]))
+
         else:
             print(f"{cmd_raw}: command not found")
 
