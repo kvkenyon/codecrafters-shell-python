46674c84ec6cee37b28d97817633e7986d159944
Kevin <kvkenyon@gmail.com>
2025-02-13 10:32:15 -0600

    exit 0

--- app/main.py
+++ app/main.py
@@ -10,6 +10,8 @@ def main():
         # Wait for user input
         sys.stdout.write("$ ")
         cmd = input()
+        if cmd == "exit 0":
+            sys.exit(0)
         if cmd not in valid_cmds:
             print(f"{cmd}: command not found")
 
